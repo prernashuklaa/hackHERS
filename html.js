84f395b797397749deaab4f72946f39304d42958
ak2846-176 <ak2846@scarletmail.rutgers.edu>
2026-02-21 14:34:34 -0500

    Add campus search input and select elements

---
+++ html.js
@@ -0,0 +1,6 @@
+<input id="campusSearch" type="text" oninput="handleCampusSearch()">
+<select id="campusSelect" onchange="renderCampusHint()">
+  <option value="">--Select a campus--</option>
+  <option value="rutgers_nb">Rutgers — New Brunswick</option>
+</select>
+<div id="campusHint"></div>
