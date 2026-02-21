76d1793a5b80c9b97b7ddd7e4713827d5a750499
ak2846-176 <ak2846@scarletmail.rutgers.edu>
2026-02-21 14:14:58 -0500

    Remove search function and related imports

--- search.js
+++ search.js
@@ -1,12 +1,1 @@
-// search.js
-import { CAMPUS_DIRECTORY } from "./campuses.js";
 
-export function findResources(userInput, campusKey = "rutgers_nb") {
-  const campus = CAMPUS_DIRECTORY[campusKey];
-  userInput = userInput.toLowerCase();
-
-  // FILTERING LOGIC GOES HERE
-  return campus.resources.filter(resource =>
-    resource.tags.some(tag => userInput.includes(tag))
-  );
-}
