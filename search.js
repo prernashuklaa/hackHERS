2d6a8423913bff375e1eca78d660d4f0d5966cd1
ak2846-176 <ak2846@scarletmail.rutgers.edu>
2026-02-21 15:11:15 -0500

    Remove search.js and its filtering function
    Remove the findResources function and related imports.

--- search.js
+++ search.js
@@ -1,13 +1,1 @@
-// search.js
-import { CAMPUS_DIRECTORY } from "./campuses.js";
 
-// Function to filter resources based on user input
-export function findResources(userInput, campusKey = "rutgers_nb") {
-  const campus = CAMPUS_DIRECTORY[campusKey];
-  userInput = userInput.toLowerCase();
-
-  // FILTERING LOGIC: only show resources whose tags match user input
-  return campus.resources.filter(resource =>
-    resource.tags.some(tag => userInput.includes(tag))
-  );
-}
